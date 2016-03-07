90c941712d899a1d17c49b0c5a4cd7b666577e8a
Kevin Osborn <kevin.osborn@gmail.com>
2016-03-07 16:25:25 -0500

    add bp_sim, and other methods

--- arduino/mmMTest/mmMTest.ino
+++ arduino/mmMTest/mmMTest.ino
@@ -4,6 +4,8 @@
  *   Kevin Osborn
  */
 #include <string.h>
+#include <SPI.h>
+
 #define PUMPPIN 9
 #define SPEAKER 6
 #define VALVE1  4
@@ -11,7 +13,9 @@
 #define LED     13
 #define BUTTON1 2
 #define BUTTON2 3
+#define SS 10
 void speakerTest();
+void GagePressureTest();
 void blink();
 void pwmPumpTest();
 void binaryPumpTest();
@@ -39,6 +43,8 @@ void menu(){
   Serial.println("1 -- Valve 1 test");
   Serial.println("2 -- Valve 2 test");
   Serial.println("S -- SpeakerTest");
+  Serial.println("G -- Gauge Pressure Test");
+  Serial.println("B -- pump, release, report pressures");
   Serial.println("h or ? -- this menu");
   
 }
@@ -55,11 +61,11 @@ void valveTest(int valve){
   delay(500);
   }
 }
-void binaryPumpTest(){
+void binaryPumpTest(long time){
     // Test pump
   Serial.println("Pump Full speed");
   digitalWrite(PUMPPIN, HIGH);
-  delay(2000);
+  delay(time);
   digitalWrite(PUMPPIN,LOW);
   blink();
 }
@@ -74,6 +80,49 @@ void pwmPumpTest(){
   digitalWrite(PUMPPIN,LOW);
   blink();
 }
+void pumpUntil (uint16_t pressure){
+  digitalWrite(PUMPPIN,HIGH);
+  while(ReadPressure() < pressure)
+    delay(10);
+  digitalWrite(PUMPPIN,LOW);
+}
+void GagePressureTest(){
+  Serial.print("[\n");
+  for (int i =0; i < 5000; i++){
+    Serial.print(ReadPressure());
+    Serial.print(",\n");
+    delay(10);
+    }
+    Serial.print("]\n");
+}
+
+// read pressure with 10 ms delay
+// and print in python array format until pressure
+void ReadPressureUntil(uint16_t pressure){
+    uint16_t reading;
+    Serial.print("[\n");
+    while ((reading = ReadPressure()) > pressure){
+      Serial.print(reading);
+      Serial.print(",\n");
+      delay(10);
+    }
+    Serial.print("]\n");
+    
+
+}
+uint16_t ReadPressure(){
+  uint16_t reading;
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider(SPI_CLOCK_DIV32); //  or 2,4,8,16,32,64,128
+  digitalWrite(SS, LOW);
+  SPI.transfer(255);
+  reading =0;
+  reading  |= SPI.transfer(255)<<8;
+  reading |= SPI.transfer(255) &0x0ff;
+  digitalWrite(SS, HIGH);
+  return reading;
+}
 void speakerTest(){
    //Test speaker
   for (int i = 500; i < 2000; i+=100)
@@ -87,17 +136,31 @@ void blink(){
   delay(1000);
   digitalWrite(LED,LOW);
 }
+void bp_sim(){
+  digitalWrite(VALVE1,HIGH); //close valve
+  pumpUntil(12000);
+  digitalWrite(VALVE1,LOW); //open valve
+  ReadPressureUntil(2800);
+  
+}
 void loop() {
   if (Serial.available()){
     char c = Serial.read();
     switch (c){
       case 'P':
-        binaryPumpTest();
+      case 'p':
+        binaryPumpTest(2000);
         break;
       case 'W':
+      case 'w':
         pwmPumpTest();
         break;
+      case 'B':
+      case 'b':
+        bp_sim();
+        break;
       case 'S':
+      case 's':
         speakerTest();
         break;
       case '1':
@@ -106,6 +169,10 @@ void loop() {
       case '2':
         valveTest(VALVE2);
         break;
+      case 'G':
+      case 'g':
+        GagePressureTest();
+        break;
       case 'M':
       case '?':
       case 'm':
@@ -113,7 +180,8 @@ void loop() {
         break;
       default:
         break;
-    }
+     }
+     menu();
   }
   if (digitalRead(BUTTON1))
   { 
