6201853670269cce0c460bb96d28d38c62a7e97d
Justin Shaw <wyojustin@gmail.com>
2016-08-13 20:04:39 -0400

    changed 'patient' to 'user'

--- scripts/control_panel/records.py
+++ scripts/control_panel/records.py
@@ -4,19 +4,19 @@ import datetime
 con = lite.connect('blood_pressure.db')
 cur = con.cursor()
 
-N_TABLE = 2
+N_TABLE = 4
 
 def create_tables():
     sqls = ['DROP TABLE IF EXISTS Sex',
-            'DROP TABLE IF EXISTS Patient',
+            'DROP TABLE IF EXISTS User',
             'DROP TABLE IF EXISTS BP',
             'CREATE TABLE Sex(sex TEXT)',
             'INSERT INTO Sex VALUES ("male")',
             'INSERT INTO Sex VALUES ("female")',
             'INSERT INTO Sex VALUES ("other")',
-            'CREATE TABLE Patient(sexid INT, user TEXT, birth DATE)',
-            'CREATE UNIQUE INDEX unq_patient ON Patient(user)',
-            'CREATE TABLE BP(user TEXT, sys INT, dia INT, day DATETIME)'
+            'CREATE TABLE User(sexid INT, name TEXT, birth DATE)',
+            'CREATE UNIQUE INDEX unq_username ON User(name)',
+            'CREATE TABLE BP(name TEXT, sys INT, dia INT, day DATETIME)'
     ]
             
     for sql in sqls:
@@ -33,10 +33,10 @@ sexes = {}
 for id, sex in cur.fetchall():
     sexes[sex] = id
 
-def add_patient(sex, user, birthday):
+def add_patient(sex, name, birthday):
     sid = sexes[sex]
     birth = str(birthday).split()[0]
-    sql = 'INSERT INTO Patient VALUES(%d, "%s", "%s")' % (sid, user, birth)
+    sql = 'INSERT INTO User VALUES(%d, "%s", "%s")' % (sid, name, birth)
     cur.execute(sql)
     con.commit()
 
@@ -47,31 +47,31 @@ def str2ymd(s):
     day = int(month)
     return year, month, day
 
-def add_result(user, sys, dia, day):
+def add_result(name, sys, dia, day):
     day = str(day)
-    sql = 'INSERT INTO BP VALUES("%s", %d, %d, "%s")' % (user, sys, dia, day)
+    sql = 'INSERT INTO BP VALUES("%s", %d, %d, "%s")' % (name, sys, dia, day)
     cur.execute(sql)
     con.commit()
 
-def getAge(user):
-    sql = 'SELECT birth FROM Patient WHERE user="%s"' % user
-    cur.execute('SELECT * FROM Patient')
+def getAge(name):
+    sql = 'SELECT birth FROM User WHERE name="%s"' % name
+    cur.execute('SELECT * FROM User')
     cur.execute(sql)
     result = cur.fetchall()
     if len(result) == 0:
-        raise ValueError("User %s, not found" % user)
+        raise ValueError("User %s, not found" % name)
     elif len(result) == 1:
         birth = result[0][0]
         year, month, day = str2ymd(birth)
         birth = datetime.datetime(year, month, day)
     else:
-        raise ValueError("Duplicate user %s" % user)
+        raise ValueError("Duplicate user name %s" % name)
         
     now = datetime.datetime.now()
     return (now - birth).days / 365.25
 
-def getBPs(user):
-    sql = 'SELECT sys, dia, day FROM BP WHERE user="%s"' % user
+def getBPs(name):
+    sql = 'SELECT sys, dia, day FROM BP WHERE name="%s"' % name
     cur.execute(sql)
     out = cur.fetchall()[0]
     day = out[2]
@@ -81,10 +81,15 @@ def getBPs(user):
             'dia': out[1],
             'day': day}
 
+def getNames():
+    sql = 'SELECT name FROM User'
+    cur.execute(sql)
+    return [l[0] for l in cur.fetcall()]
 def test():
     create_tables()
-    add_patient('male', 'justin', datetime.datetime(1970, 3, 10))
-    add_result('justin', 110, 70, datetime.datetime.now())
-    print getAge('justin')
-    print getBPs('justin')
+    add_patient('male', 'username', datetime.datetime(1970, 3, 10))
+    add_result('username', 110, 70, datetime.datetime.now())
+    print getAge('username')
+    print getBPs('username')
+
 # test()
