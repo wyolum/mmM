5603aed7efbcef13a6b245e83433fd80e4e99a62
osbock <kevin.osborn@gmail.com>
2017-08-06 17:28:29 -0400

    comment out debug code, and report proper counts from the flow sensor

--- arduino/Honeywell_test/Honeywell_test.ino
+++ arduino/Honeywell_test/Honeywell_test.ino
@@ -5,6 +5,7 @@ void setup(){
   Serial.begin(115200);
   Serial.println("Honeywell_test");
   Wire.begin();
+
 }
 
 void read_flow(byte *where){
@@ -14,22 +15,31 @@ void read_flow(byte *where){
 // end hi res start initaate read command
 //  Wire.requestFrom(SENSIRION_ADDR_1, 2);
   Wire.requestFrom(HONEYWELL_ADDR, 2);
+
   if(Wire.available() == 2){
     where[1] = Wire.read(); 
     where[0] = Wire.read(); 
   }
-  if(where[0] >> 6){
+ /* if(where[0] >> 6){
     Serial.println("status");
     read_flow(where);
+    
   }
+  */
 }
 
 byte where[2];
 uint16_t val;
 
 void loop(){
+
   read_flow(where);
+  /*
   val = (uint16_t)*where;
+  Serial.println(val);
+  */
+  //Serial.print(where[0]);Serial.print(" ");Serial.println(where[1]);
+  val = (uint16_t)(where[1] )<<8 | (uint16_t)where[0];
   Serial.println(val);
   delay(10);
 }
